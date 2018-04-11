759a35d384ce4d97c5d13bb42279b37caf139647
Sergey Kotelnikov <sergey.kotelnikov@c-map.com>
2018-04-11 16:07:04 +0300

    Fixed 1 hour time label

--- quicktimer/utils.cpp
+++ quicktimer/utils.cpp
@@ -6,13 +6,19 @@ std::string toHumanTime(std::chrono::seconds seconds) {
     if (seconds <= std::chrono::seconds(0))
         secondsCount = 0;
 
-    const int hoursPart = secondsCount / 60 / 60;
-    const int minutesPart = secondsCount / 60;
+    int hoursPart = secondsCount / 60 / 60;
+    int minutesPart = (secondsCount - hoursPart * 60 * 60) / 60;
     const int secondsPart = secondsCount - hoursPart * 60 * 60 - minutesPart * 60;
+
+    // Case - 1 hour represented as 60:00
+    if (hoursPart == 1 && minutesPart == 0 && secondsPart == 0) {
+        minutesPart = 60;
+        hoursPart = 0;
+    }
 
     QString text = QString("%1:%2").arg(minutesPart, 2, 10, QChar('0')).arg(secondsPart, 2, 10, QChar('0'));
     if (hoursPart > 0)
-        text = QString("%1:").arg(minutesPart, 2, 10, QChar('0')) + text;
+        text = QString("%1:").arg(hoursPart, 2, 10, QChar('0')) + text;
 
     return text.toStdString();
 }
